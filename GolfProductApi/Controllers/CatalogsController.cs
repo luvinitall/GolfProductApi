a1ff10fa423e5d7932343bea0c3abc7fe067405c
Christopher Perlman <christopher.perlman@gmail.com>
2018-05-28 14:54:38 -0700

    Trying to get a post to work without success

--- GolfProductApi/Controllers/CatalogsController.cs
+++ GolfProductApi/Controllers/CatalogsController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using GolfProductApi.Entities;
+using GolfProductApi.Helpers;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Query;
 using Microsoft.AspNet.OData.Routing;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace GolfProductApi.Controllers
@@ -26,7 +29,7 @@ namespace GolfProductApi.Controllers
 
         [HttpGet]
         [EnableQuery]
-        public IActionResult Get(ODataQueryOptions<Catalog> options)
+        public IActionResult Get()
         {
             try
             {
@@ -42,11 +45,16 @@ namespace GolfProductApi.Controllers
         [HttpGet]
         [EnableQuery]
         [ODataRoute("Catalogs({key})")]
-        public IActionResult Get([FromODataUri] short key, ODataQueryOptions<Catalog> options)
+        public IActionResult Get([FromODataUri] short key)
         {
             try
             {
-                throw new Exception();
+                var catalogs = _context.Catalogs.Where(c => c.CatalogId == key);
+
+                if (!catalogs.Any())
+                    return NotFound();
+
+                return Ok(SingleResult.Create(catalogs));
             }
             catch (Exception e)
             {
@@ -55,5 +63,227 @@ namespace GolfProductApi.Controllers
             }
 
         }
+
+        ////[HttpGet]
+        ////[ODataRoute("Catalogs({key})/Description")]
+        ////public virtual IActionResult GetCatalogProperty([FromODataUri] int key)
+        ////{
+        ////    var catalog = _context.Catalogs.FirstOrDefault(c => c.CatalogId == key);
+
+        ////    if (catalog == null)
+        ////        return NotFound();
+
+        ////    var propertyToGet = Request.RequestUri.Segments.Last();
+
+        ////    if (!catalog.HasProperty(propertyToGet))
+        ////        return NotFound();
+
+        ////    var propertyValue = catalog.GetValue(propertyToGet);
+
+        ////    if (propertyValue == null)
+        ////        return StatusCode(System.Net.HttpStatusCode.NoContent);
+
+
+        ////    return this.CreateOKHttpActionResult(propertyValue);
+        ////}
+
+        [HttpGet]
+        [ODataRoute("Catalogs({key})/CatalogCategories")]
+        [EnableQuery]
+        public IActionResult GetCatalogCollectionProperty([FromODataUri] int key)
+        {
+            var catalog = _context.Catalogs.Include("CatalogCategories").Where(c => c.CatalogId == key);
+
+            if (!catalog.Any())
+                return NotFound();
+
+
+            return Ok(_context.Categories.Where(f => f.CatalogCategories.Any(c => c.CatalogId == key)));
+
+
+        }
+
+        //[HttpGet]
+        //[ODataRoute("Catalogs({key})/Description/$value")]
+        //public IActionResult GetCatalogPropertyRawValue([FromODataUri] int key)
+        //{
+        //    var catalog = _context.Catalogs.FirstOrDefault(c => c.CatalogId == key);
+
+        //    if (catalog == null)
+        //        return NotFound();
+
+
+
+        //    var propertyToGet = Request.RequestUri.Segments[Request.RequestUri.Segments.Length - 2].TrimEnd('/');
+
+        //    if (!catalog.HasProperty(propertyToGet))
+        //        return NotFound();
+
+        //    var propertyValue = catalog.GetValue(propertyToGet);
+
+        //    if (propertyValue == null)
+        //        return StatusCode(System.Net.HttpStatusCode.NoContent);
+
+
+        //    return this.CreateOKHttpActionResult(propertyValue.ToString());
+        //}
+        [HttpPost]
+        public IActionResult Post(Catalog catalog)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Catalogs.Add(catalog);
+            _context.SaveChanges();
+
+            return Created(catalog);
+        }
+
+        public IActionResult Put([FromODataUri] int key, Catalog catalog)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var currentCatalog = _context.Catalogs.FirstOrDefault(c => c.CatalogId == key);
+            if (currentCatalog == null)
+                return NotFound();
+
+            catalog.CatalogId = currentCatalog.CatalogId;
+
+
+            _context.Entry(currentCatalog).CurrentValues.SetValues(catalog);
+
+            _context.SaveChanges();
+
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+
+        public IActionResult Patch([FromODataUri] int key, Delta<Catalog> patch)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+            var currentCatalog = _context.Catalogs.FirstOrDefault(c => c.CatalogId == key);
+            if (currentCatalog == null)
+                return NotFound();
+
+            patch.Patch(currentCatalog);
+            _context.SaveChanges();
+
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+
+        public IActionResult Delete([FromODataUri] int key)
+        {
+            var currentCatalog = _context.Catalogs.Include("CatalogCategories").FirstOrDefault(c => c.CatalogId == key);
+            if (currentCatalog == null)
+                return NotFound();
+
+            //Can not delete if the catalog is linked to any families;
+            if (currentCatalog.CatalogCategories.Any())
+            {
+                return new ContentResult()
+                {
+                    StatusCode = StatusCodes.Status409Conflict,
+                    Content = "This catalog is being referenced by other categories."
+                };
+
+            }
+
+            _context.Catalogs.Remove(currentCatalog);
+            _context.SaveChanges();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        //[HttpPost]
+        //[ODataRoute("Catalogs({key})/CatalogCategories/$ref")]
+        //public IActionResult CreateLinkToCatalog([FromODataUri] short key, [FromBody] Uri link)
+        //{
+        //    var currentCatalog = _context.Catalogs.Include("CatalogCategories").FirstOrDefault(c => c.CatalogId == key);
+        //    if (currentCatalog == null)
+        //        return NotFound();
+
+        //    short keyOfCategoryToAdd = Request.GetKeyValue<short>(link);
+
+        //    if (currentCatalog.CatalogCategories.Any(i => i.CatalogId == keyOfCategoryToAdd))
+        //        return BadRequest($"The family with id {keyOfCategoryToAdd} is already linked to this catalog");
+
+
+        //    var categoryLinkToAdd = _context.Categories.Include("CatalogCategories").FirstOrDefault(f => f.CategoryId == keyOfCategoryToAdd);
+
+        //    if (categoryLinkToAdd == null)
+        //        return NotFound();
+
+        //    currentCatalog.CatalogCategories.Add(new CatalogCategory() { CategoryId = keyOfCategoryToAdd, CatalogId = key });
+        //    // categoryLinkToAdd.CatalogCategories = currentCatalog;
+        //    _context.SaveChanges();
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        //[HttpPut]
+        //[ODataRoute("Catalogs({key})/CatalogCategories({relatedKey})/$ref")]
+        //public IActionResult UpdateLinkToFamily([FromODataUri] int key, [FromODataUri] int relatedKey, [FromBody] Uri link)
+        //{
+        //    //////var currentCatalog = _context.Categories.Include("Families").FirstOrDefault(c => c.CatalogId == key);
+        //    //////if (currentCatalog == null)
+        //    //////    return NotFound();
+
+        //    //////var familyToRemove = currentCatalog.Families.FirstOrDefault(f => f.FamilyId == relatedKey);
+
+        //    //////if (familyToRemove == null)
+        //    //////    return NotFound();
+
+        //    //////int keyOfFamilyToAdd = Request.GetKeyValue<int>(link);
+
+        //    //////if (currentCatalog.Families.Any(i => i.FamilyId == keyOfFamilyToAdd))
+        //    //////    return BadRequest($"The family with id {keyOfFamilyToAdd} is already linked to this catalog");
+
+
+        //    //////var familyLinkToAdd = _context.Families.Include("Catalog").FirstOrDefault(f => f.FamilyId == keyOfFamilyToAdd);
+
+        //    //////if (familyLinkToAdd == null)
+        //    //////    return NotFound();
+
+
+        //    //////currentCatalog.Families.Remove(familyToRemove);
+        //    //////_context.Families.Remove(familyToRemove);
+
+
+        //    //////currentCatalog.Families.Add(familyLinkToAdd);
+        //    //////familyLinkToAdd.Catalog = currentCatalog;
+        //    //////_context.SaveChanges();
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        //[HttpDelete]
+        //[ODataRoute("Catalogs({key})/CatalogCategories({relatedKey})/$ref")]
+        //public IActionResult DeleteLinkToFamily([FromODataUri] int key, [FromODataUri] int relatedKey)
+        //{
+        //    var currentCatalog = _context.Catalogs.Include("CatalogCategories").FirstOrDefault(c => c.CatalogId == key);
+        //    if (currentCatalog == null)
+        //        return NotFound();
+
+        //    //////var familyToRemove = currentCatalog.Families.FirstOrDefault(f => f.FamilyId == relatedKey);
+
+        //    //////if (familyToRemove == null)
+        //    //////    return NotFound();
+
+        //    //////familyToRemove.Catalog = null;
+        //    //////currentCatalog.Families.Remove(familyToRemove);
+        //    //////_context.Families.Remove(familyToRemove);
+
+        //    _context.SaveChanges();
+        //    return StatusCode(HttpStatusCode.NoContent);
+
+        //}
+
+
     }
 }
---
+++ GolfProductApi/Helpers/ODataHelper.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OData.UriParser;
+
+namespace GolfProductApi.Helpers
+{
+    public static class ODataHelpers
+    {
+        public static bool HasProperty(this object instance, string propertyName)
+        {
+            var propertyInfo = instance.GetType().GetProperty(propertyName);
+            return (propertyInfo != null);
+        }
+
+        public static object GetValue(this object instance, string propertyName)
+        {
+            var propertyInfo = instance.GetType().GetProperty(propertyName);
+            if (propertyInfo == null)
+            {
+                throw new HttpRequestException("Can't find property with name " + propertyName);
+            }
+            var propertyValue = propertyInfo.GetValue(instance, new object[] { });
+
+            return propertyValue;
+        }
+
+        public static IActionResult CreateOKHttpActionResult(this ODataController controller, object propertyValue)
+        {
+            var okMethod = default(MethodInfo);
+
+            // find the ok method on the current controller
+            var methods = controller.GetType().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (var method in methods)
+            {
+                if (method.Name == "Ok" && method.GetParameters().Length == 1)
+                {
+                    okMethod = method;
+                    break;
+                }
+            }
+
+            // invoke the method, passing in the propertyValue
+            okMethod = okMethod.MakeGenericMethod(propertyValue.GetType());
+            var returnValue = okMethod.Invoke(controller, new object[] { propertyValue });
+            return (IActionResult)returnValue;
+        }
+
+
+        ///// <summary>
+        ///// Helper method to get the odata path for an arbitrary odata uri.
+        ///// </summary>
+        ///// <param name="request">The request instance in current context</param>
+        ///// <param name="uri">OData uri</param>
+        ///// <returns>The parsed odata path</returns>
+        //public static ODataPath CreateODataPath(this HttpRequestMessage request, Uri uri)
+        //{
+        //    if (uri == null)
+        //    {
+        //        throw new ArgumentNullException(nameof(uri));
+        //    }
+           
+        //    var newRequest = new HttpRequestMessage(HttpMethod.Get, uri);
+        //    var route = request.GetRouteData().Route;
+
+        //    var newRoute = new HttpRoute(
+        //        route.RouteTemplate,
+        //        new HttpRouteValueDictionary(route.Defaults),
+        //        new HttpRouteValueDictionary(route.Constraints),
+        //        new HttpRouteValueDictionary(route.DataTokens),
+        //        route.Handler);
+
+        //    newRequest.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, request.Properties[HttpPropertyKeys.HttpConfigurationKey]);
+
+        //    var routeData = newRoute.GetRouteData(request.GetConfiguration().VirtualPathRoot, newRequest);
+        //    if (routeData == null)
+        //    {
+        //        throw new InvalidOperationException("This link is not a valid OData link.");
+        //    }
+
+        //    return newRequest.ODataProperties().Path;
+        //}
+
+        public static TKey GetKeyValue<TKey>(this HttpRequest request, Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            //get the odata path Ex: ~/entityset/key/$links/navigation
+            var odataPath = request.ToUri();
+            var keySegment = odataPath.Segments.OfType<KeySegment>().LastOrDefault();
+            if (keySegment == null)
+            {
+                throw new InvalidOperationException("This link does not contain a key.");
+            }
+
+
+            return (TKey)keySegment.Keys.Last().Value;
+        }
+
+        public static Uri ToUri(this HttpRequest request)
+        {
+            var hostComponents = request.Host.ToUriComponent().Split(':');
+
+            var builder = new UriBuilder
+            {
+                Scheme = request.Scheme,
+                Host = hostComponents[0],
+                Path = request.Path,
+                Query = request.QueryString.ToUriComponent()
+            };
+
+            if (hostComponents.Length == 2)
+            {
+                builder.Port = Convert.ToInt32(hostComponents[1]);
+            }
+
+            return builder.Uri;
+        }
+    }
+}
