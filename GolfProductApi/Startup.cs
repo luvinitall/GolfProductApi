9e0dd3f84a5d28b92ef260f01997dfb823be1746
Christopher Perlman <christopher.perlman@gmail.com>
2018-05-28 08:04:18 -0700

    Renaming file to match class, adding commented json settings if I need access to config.

--- GolfProductApi/Startup.cs
+++ GolfProductApi/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.OData.Edm;
@@ -18,6 +19,14 @@ namespace GolfProductApi
 {
     public class Startup
     {
+        //if I want to same any config settings in json, uncomment
+        //public static IConfiguration Configuration;
+
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
